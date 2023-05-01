92d56101a9771e8da7eabaf54e0ac4c9cf5ae787
Sergey Bereshpolov <bereshs@mail.ru>
2023-05-01 20:07:47 +0300

    17.7 homework

--- bonus/ImagesDownloader/src/main/java/ImagesDownloader.java
+++ bonus/ImagesDownloader/src/main/java/ImagesDownloader.java
@@ -1,2 +1,56 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.HashSet;
+
 public class ImagesDownloader {
+    public static void main(String[] args) {
+        String url = "https://skillbox.ru/";
+        HashSet<String> links = new HashSet<>();
+        try {
+            Document doc = Jsoup.connect(url).get();
+            Elements images = (Elements) doc.select("img");
+            for (Element image : images) {
+                links.add(image.attr("abs:src"));
+            }
+            int number = 0;
+            for (String link : links) {
+                String fileExtension = link.replaceAll(
+                                "^.+\\.", "")
+                        .replace("?.+$", "");
+                String filePath = "data/" + number++ + "." + fileExtension;
+                try {
+                    download(link, filePath);
+                } catch (IOException ex) {
+                    System.out.println("bad link " + link);
+                }
+            }
+            ;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void download(String link, String pathToWrite) throws IOException {
+        URLConnection connection = new URL(link).openConnection();
+        InputStream inStream = connection.getInputStream();
+        FileOutputStream outStream = new FileOutputStream(pathToWrite);
+        int element;
+        while ((element = inStream.read()) != -1) {
+            outStream.write(element);
+        }
+        outStream.flush();
+        outStream.close();
+        inStream.close();
+    }
+
+
 }
